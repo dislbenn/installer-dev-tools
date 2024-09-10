60c61d5304d8ea6722915e428c391eade990a33c
dislbenn <dbennett@redhat.com>
2024-09-10 13:44:07 -0400

    test 2
    Signed-off-by: dislbenn <dbennett@redhat.com>

--- bundle-generation/generate-charts.py
+++ bundle-generation/generate-charts.py
@@ -201,6 +201,7 @@ def copyHelmChart(destinationChartPath, repo, chart, chartVersion):
     helmTemplateOutput = subprocess.getoutput(['helm template '+ chartPath])
     yamlList = helmTemplateOutput.split('---')
     for outputContent in yamlList:
+        logging.info("output:", outputContent)
         yamlContent = yaml.safe_load(outputContent)
         if yamlContent is None:
             continue
