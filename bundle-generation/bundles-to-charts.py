046a4eef730ee451493fdd36e90086cd32289d00
dislbenn <dbennett@redhat.com>
2024-09-19 15:44:46 -0400

    added deployOnOCP condition to control logic
    Signed-off-by: dislbenn <dbennett@redhat.com>

--- bundle-generation/bundles-to-charts.py
+++ bundle-generation/bundles-to-charts.py
@@ -517,7 +517,7 @@ def injectHelmFlowControl(deployment, sizes):
 
             # If seccompProfile is present, check deployOnOCP first
             if next_line.strip() == "type: RuntimeDefault":
-                insertFlowControlIfAround(lines, i, i+1, ".Values.global.deployOnOCP")
+                insertFlowControlIfAround(lines, i-1, i+1, ".Values.global.deployOnOCP")
 
                 # After inserting deployOnOCP check, add semverCompare check
                 if "seccompProfile:" in lines[i]:
