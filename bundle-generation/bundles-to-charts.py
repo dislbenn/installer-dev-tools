ab04b842cbd4f4052eab25678d9fa3927121cf82
dislbenn <dbennett@redhat.com>
2024-10-09 16:59:52 -0400

    undo commit if fail
    Signed-off-by: dislbenn <dbennett@redhat.com>

--- bundle-generation/bundles-to-charts.py
+++ bundle-generation/bundles-to-charts.py
@@ -937,10 +937,11 @@ def main():
                 logging.error("Unable to find given channel: %s", operator.get("channel", "Channel not specified"))
                 exit(1)
 
-            if "branch" in repo["branch"]:
-                branch = repo["branch"]
-            else:
-                branch = ""
+            logging.info("repo", repo, repo["branch"])
+            # if "branch" in repo["branch"]:
+            #     branch = repo["branch"]
+            # else:
+            #     branch = ""
 
             logging.info("Reading CSV: %s ...",  csvPath)
             # Validate CSV exists
