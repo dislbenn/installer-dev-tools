3ae590890947903cb646cd4385a83f7af03ea28f
dislbenn <dbennett@redhat.com>
2024-10-31 14:00:04 -0400

    undo commit
    Signed-off-by: dislbenn <dbennett@redhat.com>

--- bundle-generation/generate-charts.py
+++ bundle-generation/generate-charts.py
@@ -329,7 +329,7 @@ def insertFlowControlIfAround(lines_list, first_line_index, last_line_index, if_
    lines_list[first_line_index] = "{{- if %s }}\n%s" % (if_condition, lines_list[first_line_index])
    lines_list[last_line_index] = "%s{{- end }}\n" % lines_list[last_line_index]
 
-def is_version_compatible(branch, min_release_version, min_backplane_version):
+def is_version_compatible(branch, min_release_version, min_backplane_version, min_ocm_version):
     # Extract the version part from the branch name (e.g., '2.12-integration' -> '2.12')
     pattern = r'(\d+\.\d+)'  # Matches versions like '2.12'
     
@@ -341,10 +341,15 @@ def is_version_compatible(branch, min_release_version, min_backplane_version):
         v = match.group(1)  # Extract the version
         branch_version = version.Version(v)  # Create a Version object
         
-        if "release" in branch:
+        if "release-ocm" in branch:
+            min_branch_version = version.Version(min_ocm_version)  # Use the minimum release version
+        
+        elif "release" in branch:
             min_branch_version = version.Version(min_release_version)  # Use the minimum release version
-        elif "backplane" or "mce" in branch:
+
+        elif "backplane" in branch or "mce" in branch:
             min_branch_version = version.Version(min_backplane_version)  # Use the minimum backplane version
+
         else:
             logging.error(f"Unrecognized branch type for branch: {branch}")
             return False
@@ -410,7 +415,7 @@ def injectHelmFlowControl(deployment, branch):
             prev_line = lines[i-1]
             if next_line.strip() == "type: RuntimeDefault" and "semverCompare" not in prev_line:
                 insertFlowControlIfAround(lines, i, i+1, "semverCompare \">=4.11.0\" .Values.hubconfig.ocpVersion")
-                if is_version_compatible(branch, '2.13', '2.7'):
+                if is_version_compatible(branch, '2.13', '2.7', '2.12'):
                     insertFlowControlIfAround(lines, i, i+1, ".Values.global.deployOnOCP")
 
         a_file = open(deployment, "w")
