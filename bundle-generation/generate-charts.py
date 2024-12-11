b782692400588c245ec28fd10d3349a2960c7442
dislbenn <dbennett@redhat.com>
2024-12-11 09:49:46 -0500

    undo commit
    Signed-off-by: dislbenn <dbennett@redhat.com>

--- bundle-generation/generate-charts.py
+++ bundle-generation/generate-charts.py
@@ -211,11 +211,12 @@ def copyHelmChart(destinationChartPath, repo, chart, chartVersion):
 
         name = yamlContent.get('metadata', {}).get('name', '').lower()
         kind = yamlContent.get('kind', '').lower()
+        logging.debug(f"Extracted name: '{name}', kind: '{kind}'.")
 
         yamlFileName = f"{name}-{kind}" if name else kind
         newFileName = yamlFileName + '.yaml'
         newFilePath= os.path.join(destinationTemplateDir, newFileName)
-        logging.info(f"Generated file name: '{newFileName}'.")
+        logging.info(f"Generated file: '{newFileName}'.\n--------------------")
         
         try:
             with open(newFilePath, "w") as f:
