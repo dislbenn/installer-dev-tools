856848fb7659970916cb8c5f6d5d08ddd306398d
dislbenn <dbennett@redhat.com>
2024-12-11 09:53:08 -0500

    undo commit
    Signed-off-by: dislbenn <dbennett@redhat.com>

--- bundle-generation/generate-charts.py
+++ bundle-generation/generate-charts.py
@@ -675,8 +675,6 @@ def addCRDs(repo, chart, outputDir):
 
         filepath = os.path.join(crdPath, filename)
         with open(filepath, 'r') as f:
-            logging.info(f"filepath {filepath}")
-
             resourceFile = yaml.safe_load(f)
 
         if resourceFile["kind"] == "CustomResourceDefinition":
