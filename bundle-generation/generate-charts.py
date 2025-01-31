8bd4a4d11710955d278d93ae28461f2b32de9c15
dislbenn <dbennett@redhat.com>
2025-01-31 10:04:20 -0500

    undo commit
    Signed-off-by: dislbenn <dbennett@redhat.com>

--- bundle-generation/generate-charts.py
+++ bundle-generation/generate-charts.py
@@ -251,7 +251,7 @@ def copyHelmChart(destinationChartPath, repo, chart, chartVersion):
     logging.info(f"helm template output {helmTemplateOutput}")
     yamlList = helmTemplateOutput.split('---')
     for outputContent in yamlList:
-        yamlContent = yaml.safe_load_all(outputContent)
+        yamlContent = list(yaml.safe_load_all(outputContent))
         if yamlContent is None:
             logging.warning("Skipped empty or invalid YAML content during template processing")
             continue
