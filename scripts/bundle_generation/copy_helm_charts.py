5447bc23f60f90bc8ccc953b7c0824293d3f1011
dislbenn <dbennett@redhat.com>
2025-02-06 15:35:46 -0500

    undo commit
    Signed-off-by: dislbenn <dbennett@redhat.com>

--- scripts/bundle_generation/copy_helm_charts.py
+++ scripts/bundle_generation/copy_helm_charts.py
@@ -18,10 +18,11 @@ on configuration data provided in `copy-config.yaml`. It performs the following 
 
 Requirements:
 - Python 3.6 or higher
-- External libraries: `argparse`, `os`, `shutil`, `yaml`, `logging`, `subprocess`, `gitpython`
+- External libraries: 
+    `argparse`, `logging`, `os`, `shutil`, `sys`, `coloredlogs`, `yaml`, `git`
 
 Usage:
-    python3 copy-helm-charts.py --destination <output_directory>
+    python3 copy_helm_charts.py --destination <output_directory>
 
 Arguments:
     --destination: The directory where the generated Helm charts will be stored.
@@ -39,7 +40,7 @@ import sys
 import coloredlogs
 import yaml
 
-from git import Repo
+from git import Repo, GitCommandError
 from validate_csv import *
 
 # Configure logging with coloredlogs
@@ -146,10 +147,13 @@ def add_crds(repo, chart, output_dir):
             shutil.copyfile(file_path, os.path.join(destination_path, filename))
 
 def main():
-    """_summary_
     """
+    Main function that orchestrates the execution of various tasks
+    related to Helm chart creation, such as copying CRDs.
+    """
+
     logging.basicConfig(level=logging.DEBUG)
-    logging.info("Script started.")
+    logging.info("Starting script execution: Copy Helm chart process.")
 
     ## Initialize ArgParser
     parser = argparse.ArgumentParser()
@@ -163,23 +167,18 @@ def main():
     copy_config_file_path = os.path.join(script_dir, "copy-config.yaml")
 
     # Check if the configuation file exists
-    if os.path.exists(copy_config_file_path):
-        logging.info("Loading configuration from: %s", copy_config_file_path)
-        try:
-            with open(copy_config_file_path, 'r', encoding="utf-8") as file:
-                config = yaml.safe_load(file)
-                logging.info("Configuration loaded successfully")
-
-        except FileNotFoundError as err:
-            logging.error("Configuration file not found: %s", err, exc_info=True)
-            sys.exit(1)
-
-        except yaml.YAMLError as err:
-            logging.error("Error parsing the YAML configuration file: %s", err, exc_info=True)
-            sys.exit(1)
-
-    else:
+    if not os.path.exists(copy_config_file_path):
         logging.warning("Configuration file not found: %s", copy_config_file_path)
+        sys.exit(1)
+
+    logging.info("Loading configuration from: %s", copy_config_file_path)
+    try:
+        with open(copy_config_file_path, 'r', encoding="utf-8") as file:
+            config = yaml.safe_load(file)
+            logging.info("Configuration loaded successfully")
+
+    except (FileNotFoundError, yaml.YAMLError) as err:
+        logging.error("Error with the configuration file: %s", err, exc_info=True)
         sys.exit(1)
 
     # Loop through each repo in the config.yaml
@@ -187,23 +186,31 @@ def main():
         repo_name, repo_github_ref = repo.get("repo_name"), repo.get("github_ref")
 
         if not repo_name or not repo_github_ref:
-            logging.warning(
-                "Skipping entry due to missing 'name' or 'github_ref' field in configuration")
+            logging.warning("Skipping entry due to missing 'name' or 'github_ref' field")
             continue
 
-        # Define path to clone repository
         tmp_repo_path = os.path.join(script_dir, "tmp", repo_name)
-        logging.debug("Repository will be cloned to: %s", tmp_repo_path)
+        logging.debug("Cloning repository to: %s", tmp_repo_path)
 
-        # If path exists, remove and re-clone
+        # Remove existing repo if exists
         if os.path.exists(tmp_repo_path):
             logging.warning(
                 "Repository already exists at '%s'. Deleting to re-clone.", tmp_repo_path)
             shutil.rmtree(tmp_repo_path)
 
-        repository = Repo.clone_from(repo_github_ref, tmp_repo_path) # Clone repo to above path
-        if 'branch' in repo:
-            repository.git.checkout(repo['branch']) # If a branch is specified, checkout that branch
+        # Clone repo and checkout branch if specified
+        repository = Repo.clone_from(repo_github_ref, tmp_repo_path)
+        branch = repo.get("branch", "main")
+
+        try:
+            logging.info("Checking out branch '%s' for repository at '%s'",
+                branch, tmp_repo_path)
+            repository.git.checkout(repo['branch'])
+
+        except GitCommandError as err:
+            logging.error("Failed to checkout branch '%s' for repository at '%s': %s",
+                branch, tmp_repo_path, err, exc_info=True)
+            sys.exit(1)
 
         # Loop through each operator in the repo identified by the config
         for chart in repo["charts"]:
@@ -211,7 +218,7 @@ def main():
 
             if not chart_name:
                 logging.critical(
-                    "Helm chart configuration is missing a 'name' field. Aborting chart generation.")
+                    "Helm chart configuration is missing 'name' field. Aborting chart generation")
                 sys.exit(1)
 
             logging.info("Helm Chartifying - %s!\n", chart_name)
@@ -233,12 +240,10 @@ def main():
     logging.info("All repositories and operators processed successfully")
 
     # Perform cleanup
-    logging.info("----------------------------------------------------------------")
     logging.info("Starting cleanup process...")
     tmp_dir = os.path.join(script_dir, "tmp")
     shutil.rmtree(tmp_dir, ignore_errors=True)
     logging.info("Cleanup completed. Temporary files removed from '%s'", tmp_dir)
-    logging.info("----------------------------------------------------------------")
 
     logging.info("Script execution completed successfully")
 
