856a40d65402d29c4a714d2cad878276f4fc3017
dislbenn <dbennett@redhat.com>
2025-02-17 15:42:56 -0500

    undo commit
    Signed-off-by: dislbenn <dbennett@redhat.com>

--- bundle-generation/bundles-to-charts.py
+++ bundle-generation/bundles-to-charts.py
@@ -1287,7 +1287,7 @@ def main():
     # Loop through each repo in the config.yaml
     for repo in config:
         # We support two ways of getting bundle input:
-
+        #
         # - Picking up already generated input from a Github repo
         #
         #   Entries for this approach include a "github_ref" property specifying the
@@ -1307,19 +1307,29 @@ def main():
         #   to obtain bundle input info.
 
         csv_path = ""
-        repo_name = repo.get("repo_name")
+        repo_name, github_ref = repo.get("repo_name"), repo.get("github_ref")
+        logging.info("Processing repository: %s", repo_name)
 
-        if "github_ref" in repo:
-            logging.info("Cloning: %s", repo_name)
+        if github_ref:
+            logging.info("Cloning repository: %s from %s", repo_name, github_ref)
+            repo_path = os.path.join(SCRIPT_DIR, "tmp", repo_name)
 
-            repo_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp/" + repo["repo_name"]) # Path to clone repo to
-            if os.path.exists(repo_path): # If path exists, remove and re-clone
-                logging.warning("Repo path already exist, removing", repo_path)
+            # Remove existing repo paths if they exist and then reclone.
+            if os.path.exists(repo_path):
+                logging.warning("Repo path '%s' already exist. Removing old clone", repo_path)
                 shutil.rmtree(repo_path)
 
-            repository = Repo.clone_from(repo["github_ref"], repo_path) # Clone repo to above path
-            if 'branch' in repo:
-                repository.git.checkout(repo['branch']) # If a branch is specified, checkout that branch
+            try:
+                repository = Repo.clone_from(github_ref, repo_path) # Clone the repository
+
+                branch = repo.get("branch")
+                if branch:
+                    logging.info("Checking out branch: '%s'", branch)
+                    repository.git.checkout(branch)
+                    logging.info("Checked out branch: '%s' successfully", branch)
+
+            except Exception as e:
+                logging.error("Failed to clone repository: '%s' from '%s': %s", repo_name, github_ref, e)
 
             # This is only used for hub sizing.
             # sizesyaml = repo_path + "/bundle/manifests/sizes.yaml"
